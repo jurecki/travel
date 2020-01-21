5d67b23c50a60ce8815755b5648f32b4e642dc68
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-01-21 15:02:16 +0100

    Routing fixed

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, addTags,removeTags} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  addTags: tags => dispatch(addTags(([tags]))),
+  removeTags: tags => dispatch(removeTags(tags)),
+  changeDuration: (type, value) => dispatch(changeDuration({type,value})),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,56 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+// TODO - add other action types
+export const ADD_TAGS = createActionName('ADD_TAGS');
+export const REMOVE_TAGS = createActionName('ADD_TAGS');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+
+// TODO - add other action creators
+export const addTags = payload => ({ payload, type: ADD_TAGS });
+export const removeTags = payload => ({payload, type: REMOVE_TAGS});
+
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE:
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+    // TODO - handle other action types
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: action.payload,
+      };
+
+    case ADD_TAGS:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };  
+
+    case REMOVE_TAGS:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };  
+    default:
+      return statePart;
+  }
+}
