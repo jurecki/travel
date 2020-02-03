a8f16b820101783cabebb40dbd08ca523c9c03c7
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-02-03 22:55:26 +0100

    Component HappyHourAdd in TDD added

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styles from './HappyHourAd.scss';
 
 
 class HappyHourAd extends React.Component {
+
   static propTypes = {
     title: PropTypes.node,
+    promoDescription: PropTypes.node,
   };
 
   getCountdownTime(){
@@ -19,15 +21,36 @@ class HappyHourAd extends React.Component {
     return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
   }
 
+  constructor(){
+    super();
+
+    /* run this.forceUpdate() every second */
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+
+  }
+
 
   render() {
-    const { title } = this.props;
-    return (
-      <div>
-        <h3 className='title'>{title}</h3>
-        <div className='promoDescription'>{this.getCountdownTime()}</div>
-      </div>
-    );
+    const { title, promoDescription } = this.props;
+    const promoTime = this.getCountdownTime();
+    if (promoTime > (23*60*60)) {
+      return (
+        <div className={styles.component}>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.promoDescription}>{promoDescription}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.component}>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.promoDescription}>{this.getCountdownTime()}</div>
+        </div>
+      );
+    }
+    
   }
 }
 
